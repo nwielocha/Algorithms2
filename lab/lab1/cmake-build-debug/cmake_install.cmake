853867c7a417835721ba3e99ab19639956d8e0d9
Nicolas Wielocha <nicolas@wielocha.pl>
2022-03-15 13:04:46 +0100

    initial commit

--- lab/lab1/cmake-build-debug/cmake_install.cmake
+++ lab/lab1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/nicolaswielocha/UG/semestr_4/algorytmy/lab
+# Install script for directory: /Users/nicolaswielocha/UG/semestr_4/algorytmy/lab/lab1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/nicolaswielocha/UG/semestr_4/algorytmy/lab/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/nicolaswielocha/UG/semestr_4/algorytmy/lab/lab1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ lab/lab1/main.cpp
@@ -0,0 +1,18 @@
+// Czarnych musi być wszedzie tyle samo
+// wartownicy też wliczaja sie jako czarne
+// 1. każdy węzeł mam przypisany kolor: czerwony lub czarny
+// 2. korzeń jest czarny
+// 3. liście są czarne; przyjmujemy wariant drzewa z
+// wartownikami - liśćmi są wartownicy
+// 4. czerwony węzeł nie może mieć czerwonego syna
+// 5. na każdej ścieżce od korzenia do liści jest tyle samo
+// czarnych węzłów
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
