176fa0f9bc2902623ff5ce7d7eb3b63e03d69876
Nicolas Wielocha <nicolas@wielocha.pl>
2022-03-15 13:32:20 +0100

    initial commit

--- lab/lab1/main.cpp
+++ lab/lab1/main.cpp
@@ -12,6 +12,41 @@
 
 using namespace std;
 
+struct Node {
+    bool color;
+    int key;
+    Node *left, *right, *p;
+};
+
+struct Node *root;
+
+void leftRotate(Node *node) {
+    Node *x = node->right;
+    Node *y = x->left;
+
+    if (y->left != nullptr)
+        y->left->p = x;
+
+    y->p = x->p;
+
+    if (x->p == nullptr)
+        root = y;
+    else if (x == x->p->left)
+        x->p->left = y;
+    else
+        x->p->right = y;
+
+    y->left = x;
+    x->p = y;
+}
+
+void rightRotate(Node *node) {
+    Node *x = node->left;
+    Node *y = x->right;
+
+    
+}
+
 int main() {
 
     return 0;
