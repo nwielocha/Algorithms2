ea5d1b1c9d5e6e507ed5c8f1c4b978a5e5951751
Nicolas Wielocha <nicolas@wielocha.pl>
2022-04-01 11:37:44 +0200

    utworzenie projektu

---
+++ lab/lab3/main.py
@@ -0,0 +1,6 @@
+# Przygotuj program znajdujący najdłuższy wspólny podciąg korzystający z programowania dynamicznego.
+# Program powinien dać do wyboru: odczyt ciągów wejściowych z konsoli lub z dwóch różnych plików tekstowych.
+# Program powinien drukować tablicę pomocniczą wraz z odpowiednim oznaczeniem przejść (ta funkcjonalność przyda się na sprawdzianie)
+# Zadanie należy przesłać najpóźniej do najbliższej niedzieli włącznie na adres bartlomiej.pawelski@ug.edu.pl
+
+# Program przygotowany przez: Nicolas Wielocha, Michalina Całus, Maciej Cymański, Mateusz Redzimski
